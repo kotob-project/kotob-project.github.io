ac4ba8ef885a7f32af58c6e9f9125d20bfba9e20
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-30 08:55:57 +0900

    feat: ルーティング設定を整理し、HomeおよびDownloadコンポーネントの表示を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/header';
-import { Home } from './pages/Home'; // TOPページの中身をここに移したと想定
-import { Download } from './pages/Download';
+import { routes } from './routes';
+
 
 function App() {
+  const filterdRoutes = routes.filter((route) => route.isActive === true);
   return (
     <Router>
       <div style={{ backgroundColor: '#000', color: '#fff', minHeight: '100vh', fontFamily: 'ui-monospace', overflowY: 'hidden'}}>
         <Header />
         
         <Routes>
-          {/* URLが "/" のときは Homeコンポーネントを表示 */}
-          <Route path="/" element={<Home />} />
-          
-          {/* URLが "/download" のときは Downloadコンポーネントを表示 */}
-          <Route path="/download" element={<Download />} />
-          
-          {/* URLが "/contributors" のときは（作っていれば）ここに追加 */}
-          {/* <Route path="/contributors" element={<Contributors />} /> */}
+          {filterdRoutes.map((route) => (<Route {...route}/>))}
         </Routes>
       </div>
     </Router>
